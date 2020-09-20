bac18e287423a7319b4524bcd2a0fd7b41632c53
Amit Nanda <amitnanda@live.com>
2020-09-21 04:56:42 +0530

    Minimum Edit Distane

--- DP/src/MinimumEditDistance.java
+++ DP/src/MinimumEditDistance.java
@@ -3,12 +3,36 @@
  * ALlowed operations are delete , replace add
  */
 public class MinimumEditDistance {
+
+//    public static int insertCost = 1;
+//    public static int delCost = 1;
+//    public static int replaceCost = 1;
+
     public static int minimumEditDist(String str1, String str2){
-        return 0;
+        int distCost[][] = new int[str1.length()+1][str2.length()+1];
+
+        for(int i = 0; i <= str1.length(); ++i){
+            for(int j =0 ; j <= str2.length(); ++j) {
+                if(i == 0 && j == 0) distCost[i][j]=0;
+                else if(i== 0 ) {
+                    distCost[0][j] = j;
+
+                } else if(j==0) {
+                    distCost[i][0] = i;
+                } else {
+                    if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                        distCost[i][j] = distCost[i-1][j-1];
+                    } else {
+                        distCost[i][j] = Math.min(distCost[i-1][j-1] + 1, Math.min(distCost[i-1][j]+1, distCost[i][j-1]+1));
+                    }
+                }
+            }
+        }
+        return distCost[str1.length()][str2.length()];
     }
     
     public static void main(String args[]){
-        String str1 ="", str2 = "";
+        String str1 ="mon", str2 = "rtue";
         System.out.println("The minimum cost to convert string from on string to other is : "+ minimumEditDist(str1, str2));
     }
 }
