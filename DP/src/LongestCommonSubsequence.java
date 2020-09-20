fc98808dde405d8b7bb70dc89979c5b778d066ab
Amit Nanda <amitnanda@live.com>
2020-09-21 02:02:50 +0530

    minimum edit distance

--- DP/src/LongestCommonSubsequence.java
+++ DP/src/LongestCommonSubsequence.java
@@ -9,7 +9,7 @@
  */
 public class LongestCommonSubsequence {
 
-	public static void  lcs(String str1, String str2) {
+	public static int  lcs(String str1, String str2) {
 		int lcs[][] = new int[str1.length()+1][str2.length()+1];
 		for(int i = 0; i<=str1.length(); ++i) {
 			for(int j =0 ;j<=str2.length(); ++j) {
@@ -23,19 +23,22 @@ public class LongestCommonSubsequence {
 			}
 		}
 		int len =  lcs[str1.length()][str2.length()];
-		String str = "";
-		int r = str1.length(), c= str2.length();
-		for(int i= len;i>0;i--) {
-			while(true) {
-				
-			}
-		}
+		return len;
+//		String str = "";
+//		int r = str1.length(), c= str2.length();
+//		for(int i= len;i>0;i--) {
+//			if((lcs[r][c] > lcs[r-1][c-1]) && (lcs[r][c] > lcs[r][c-1] )&& (lcs[r][c] > lcs[r-1][c])){
+//				 str = str + str1.charAt(r-1);
+//			} else {
+//
+//			}
+//		}
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str1= "Monday",str2 = "ofdcykajsyMeeoeentdyyayy";
-		//System.out.print("Length of longest common subsequence is : " + lcs(str2, str1));
+		System.out.print("Length of longest common subsequence is : " + lcs(str2, str1));
 		
 	}
 
---
+++ DP/src/MinimumEditDistance.java
@@ -0,0 +1,14 @@
+/**
+ * Given two strings find minimum cost to convert one string to other
+ * ALlowed operations are delete , replace add
+ */
+public class MinimumEditDistance {
+    public static int minimumEditDist(String str1, String str2){
+        return 0;
+    }
+    
+    public static void main(String args[]){
+        String str1 ="", str2 = "";
+        System.out.println("The minimum cost to convert string from on string to other is : "+ minimumEditDist(str1, str2));
+    }
+}
